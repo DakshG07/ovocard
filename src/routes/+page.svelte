906a84e1e60c76e80d74b63954174e80bfc0657c
Daksh Gupta <acedaksh07@gmail.com>
2025-04-18 19:10:31 -0400

    feat: gh pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+export const prerender = true;
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
