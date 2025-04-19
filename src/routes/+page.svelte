8725f349d1a4208d3fa320e6e70537332a0cd8b8
Daksh Gupta <acedaksh07@gmail.com>
2025-04-18 21:35:07 -0400

    test: update page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 export const prerender = true;
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to Vocard</h1>
 <p>
     Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
     documentation
